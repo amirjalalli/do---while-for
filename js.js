76b1a1a8c17a6d3174c905bb31dbe4bdc130004c
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-29 22:39:55 +0330

    do-while-for-

--- js.js
+++ js.js
@@ -1,10 +1,30 @@
-var userNumber = 0
-var sum = null
-var plus = 0
+// var num1 = Number(prompt("first number"))
+// var num2 = Number(prompt("second number"))
 
-while (userNumber != -1) {
-    plus++
-    sum += (userNumber / plus)
-    userNumber = Number(prompt('enter your number:...\nif you want the sum number enter \'-1\''))
-}
-alert(sum)
+// var pow = 1 
+// for (var i = 0; i < num2 ; i++){
+//     pow = pow * num1
+// }
+// alert(pow);
+// ///////////////////////////////////while
+// var num1 = Number(prompt("first number"))
+//  var num2 = Number(prompt("second number"))
+//  var pow = 1
+// var i = 0
+//  while(i < num2){
+//      pow = pow * num1
+//      i++
+//  }
+//  alert(pow);
+
+
+var num1 = Number(prompt("first number"))
+ var num2 = Number(prompt("second number"))
+ var pow = 1
+var i = 0
+
+do{
+ pow = pow *num1
+ i++
+}while(i < num2)
+alert(pow);
